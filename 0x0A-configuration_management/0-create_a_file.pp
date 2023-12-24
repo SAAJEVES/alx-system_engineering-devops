d706e9ef66fd1fb94a8d90f19db5ba26e2c769bb
SAAJEVES <eveshofase@gmail.com>
2023-12-24 15:19:20 +0100

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file in /tmp
+
+file {
+  '/tmp/school':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => "I love Puppet\n",
+}
